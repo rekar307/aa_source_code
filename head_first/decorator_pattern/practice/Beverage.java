ccfeb9956c6ae8098fcfba497755898108dacf76
rekar307 <rekar307@gmail.com>
2024-08-26 22:23:40 +0900

    [HeadFirst] decorator: add Practice

---
+++ head_first/decorator_pattern/practice/Beverage.java
@@ -0,0 +1,9 @@
+public abstract class Beverage {
+    String description = "Unknown";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract double cost();
+}
---
+++ head_first/decorator_pattern/practice/DeCaffein.java
@@ -0,0 +1,9 @@
+public class DeCaffein extends Beverage {
+    public DeCaffein() {
+        description = "DeCaffein";
+    }
+
+    public double cost() {
+        return 1.05;
+    }
+}
